c0de097c72e4d8090540887ade7e349964db1a0b
Leonardo Valbuena <leov.calderon@gmail.com>
2019-04-14 13:08:16 -0500

    desde leo mac

--- Navegacion/tercero/PtsViewController.swift
+++ Navegacion/tercero/PtsViewController.swift
@@ -13,7 +13,7 @@ class PtsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // desde mi leo Do any additional setup after loading the view.
     }
     
 
